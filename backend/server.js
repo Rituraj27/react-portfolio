8600728c05b9c1c279f3a710dd914ad186cc9b18
Rituraj27 <ri2rajhao1@gmail.com>
2025-07-27 23:36:16 +0530

    removed env file

--- backend/server.js
+++ backend/server.js
@@ -14,9 +14,6 @@ app.use(
 
 const port = process.env.PORT || 5000;
 
-console.log(process.env.EMAIL_PASS);
-console.log(process.env.EMAIL_USER);
-
 app.post('/send', async (req, res) => {
   const { email, name, message } = req.body;
 
@@ -46,3 +43,21 @@ app.post('/send', async (req, res) => {
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
 });
+
+// app.use((req, res, next) => {
+//   if (
+//     process.env.NODE_ENV === 'production' &&
+//     req.headers['x-forwarded-proto'] !== 'https'
+//   ) {
+//     return res.redirect(`https://${req.headers.host}${req.url}`);
+//   }
+//   next();
+// });
+
+// import rateLimit from 'express-rate-limit';
+
+// const limiter = rateLimit({
+//   windowMs: 15 * 60 * 1000, // 15 mins
+//   max: 50, // limit each IP to 50 requests per window
+// });
+// app.use('/send', limiter);
